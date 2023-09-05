a302ccb5e3cf9e3c3a916e1a8f952951a6aaec12
Mert Tunçer <merttuncer@Mert-MBP.home>
2023-09-05 11:06:16 +0300

    add edit and delete methods && add notification

--- routes/web.php
+++ routes/web.php
@@ -36,6 +36,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('chirps', ChirpController::class)->only(['index', 'store'])->middleware('auth')->middleware(['auth', 'verified']);
+Route::resource('chirps', ChirpController::class)->middleware('auth')->middleware(['auth', 'verified']);
 
 require __DIR__ . '/auth.php';
